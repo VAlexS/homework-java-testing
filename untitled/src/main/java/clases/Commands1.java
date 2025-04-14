9a0b5cb9bce3fc354f076f4839abf226dfef1524
peterParker79 <trypeterparker@gmail.com>
2025-04-14 18:55:14 +0200

    add Commands1 with method enroll

---
+++ untitled/src/main/java/clases/Commands1.java
@@ -0,0 +1,29 @@
+package clases;
+
+import java.util.Map;
+
+public class Commands1 {
+
+    /*
+    enroll the student specified in the corresponding course
+     */
+    public void enroll(String studentId,
+                     String courseId,
+                     Map<String, Student> studentMap,
+                     Map<String, Course> courseMap) throws IllegalArgumentException {
+
+    {
+        if(studentMap.containsKey(studentId)) {
+            Student studentEnrolled = studentMap.get(studentId);
+        } else {
+            throw new IllegalArgumentException("Id de estudiante no existe.");
+        }
+
+        if (courseMap.containsKey(courseId)) {
+            Course course = courseMap.get(courseId);
+        }else {
+            throw new IllegalArgumentException("Id de curso no existe.");
+
+
+    }
+}
