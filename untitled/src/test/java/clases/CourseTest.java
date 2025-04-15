a595ead77a28825d58a21aa5ff3e6209d5445432
Víctor Sanz <vsanzc@alumnos.nebrija.es>
2025-04-15 12:07:47 +0200

    Test curso finalizado

--- untitled/src/test/java/clases/CourseTest.java
+++ untitled/src/test/java/clases/CourseTest.java
@@ -24,11 +24,68 @@ class CourseTest {
     }
 
     @Test
+    @DisplayName("Le cambio el profesor asociado al curso")
     void changeTeacher(){
         course.setTeacher(new Teacher("Hector", 500));
 
         assertEquals("Hector", course.getTeacher().getName());
+        assertEquals(500, course.getTeacher().getSalary());
     }
+
+    @Test
+    @DisplayName("Modifico el precio al curso")
+    void changePriceCourse(){
+        course.setPrice(24.56);
+
+        assertEquals(24.56, course.getPrice());
+    }
+
+    @Test
+    @DisplayName("Asigno el dinero ganado")
+    void setMoneyEarned(){
+        course.setMoney_earned(800);
+
+        assertEquals(800, course.getMoney_earned());
+    }
+
+    @Test
+    @DisplayName("Le mando un precio negativo para generar un error")
+    void priceNegative(){
+        assertThrows(IllegalArgumentException.class, () -> course.setPrice(-400));
+    }
+
+    @Test
+    @DisplayName("Le mando una cantidad negativa de dinero ganado")
+    void moneyEarnedWithNegativeValue(){
+        assertThrows(IllegalArgumentException.class, () -> course.setMoney_earned(-800));
+    }
+
+    @Test
+    @DisplayName("Comparo un curso con otro curso, que deberian de coincidir")
+    void compareCoursesOne(){
+        Course course2 = new Course("Java-backend", 20.3, new Teacher("Marcel", 500));
+
+        assertTrue(course.equals(course2));
+    }
+
+    @Test
+    @DisplayName("Comparo un curso con otro que no deberia de coincidir porque le paso un profesor distinto")
+    void compareCoursesTwo(){
+        Course course2 = new Course("Java-backend", 20.3, new Teacher("Hector", 500));
+
+        assertFalse(course.equals(course2));
+    }
+
+    @Test
+    @DisplayName("Comparo un curso con otro que no deberia de coincidir porque es un curso diferente")
+    void compareCoursesThree(){
+        Course course2 = new Course("Databases SQL", 20.3, new Teacher("Marcel", 500));
+
+        assertFalse(course.equals(course2));
+    }
+
+
+
 
 
 
